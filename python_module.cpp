59050ec992267f95d0e622fdb60f832c02bbaa19
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-03 10:55:52 -0700

    SO(2)

--- python_module.cpp
+++ python_module.cpp
@@ -1,3 +1,4 @@
+#include <SO2.h>
 #include <SO3.h>
 #include <SE3.h>
 #include <pybind11/pybind11.h>
@@ -11,7 +12,54 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(geometry, m)
 {
-  m.doc() = "Python binding module for SO3/SE3.";
+  m.doc() = "Python binding module for SO2/SO3/SE3.";
+  
+  py::class_<SO2d>(m, "SO2")
+    .def_static("random", &SO2d::random)
+    .def_static("identity", &SO2d::identity)
+    .def_static("fromAngle", &SO3d::fromAngle)
+    .def_static("fromEuler", &SO3d::fromEuler)
+    .def_static("fromR", &SO3d::fromR)
+    .def_static("fromTwoUnitVectors", &SO3d::fromTwoUnitVectors)
+    .def_static("fromQuat", static_cast<SO3d (*)(const double, const double, const double, const double)>(&SO3d::fromQuat), "Instantiate SO3 from quaternion fields")
+    .def_static("fromQuat", static_cast<SO3d (*)(const Matrix<double,4,1> &)>(&SO3d::fromQuat), "Instantiate SO3 from a vector of quaternion fields")
+    .def(py::init())
+    .def(py::init<const Ref<const Matrix<double,4,1>>>())
+    .def(py::init<const SO3d &>())
+    .def("w", static_cast<double& (SO3d::*)(void)>(&SO3d::w), "Write access to w.")
+    .def("x", static_cast<double& (SO3d::*)(void)>(&SO3d::x), "Write access to x.")
+    .def("y", static_cast<double& (SO3d::*)(void)>(&SO3d::y), "Write access to y.")
+    .def("z", static_cast<double& (SO3d::*)(void)>(&SO3d::z), "Write access to z.")
+    .def("array", &SO3d::array)
+    .def("normalize", &SO3d::normalize)
+    .def("normalized", &SO3d::normalized)
+    .def("R", &SO3d::R)
+    .def("inverse", &SO3d::inverse)
+    .def("invert", &SO3d::invert)
+    .def("roll", &SO3d::roll)
+    .def("pitch", &SO3d::pitch)
+    .def("yaw", &SO3d::yaw)
+    .def("toEuler", &SO3d::toEuler)
+    .def("qMatLeft", &SO3d::qMatLeft)
+    .def(py::self * py::self)
+    .def(py::self * Matrix<double,3,1>())
+    .def(py::self + Matrix<double,3,1>())
+    .def(py::self - py::self)
+    .def(py::self * float())
+    .def(float() * py::self)
+    .def(py::self / float())
+    .def_static("hat", &SO3d::hat)
+    .def_static("vee", &SO3d::vee)
+    .def_static("log", &SO3d::log)
+    .def_static("Log", &SO3d::Log)
+    .def_static("exp", &SO3d::exp)
+    .def_static("Exp", &SO3d::Exp)
+    .def("__repr__",
+      [](const SO3d &q) {
+        return "SO(3): [ " + std::to_string(q.w()) + ", " + std::to_string(q.x()) +
+          "i, " + std::to_string(q.y()) + "j, " + std::to_string(q.z()) + "k]";
+      }
+    );
   
   py::class_<SO3d>(m, "SO3")
     .def_static("random", &SO3d::random)
